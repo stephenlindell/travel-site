5a519c3ccd27195f4c877594e2f0f031e04e5a15
Stephen Lindell <stephenlindell@MacBook-Pro.local>
2019-02-24 22:14:42 -0500

    fixed issue with modernizr not working

--- gulp/tasks/modernizr.js
+++ gulp/tasks/modernizr.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),
 modernizr = require('gulp-modernizr');
 
 gulp.task('modernizr', function() {
-  return gulp.src(['./app/asets/styles/**/*.css', './app/assets/scripts/**/*.js'])
-  .pipe(modernizr({
-    "options": [
-      "setClasses"
-    ]
-  }))
-  .pipe(gulp.dest('./app/temp/scripts'));
-})
+  return gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
+    .pipe(modernizr({
+      "options": [
+        "setClasses"
+      ]
+    }))
+    .pipe(gulp.dest('./app/temp/scripts/'));
+});
